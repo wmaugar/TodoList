1fb870ae4699f2b0d98ebff329ee549a471d9a38
Willy Mauricio <wmgarciaporcel@gmail.com>
2022-09-25 07:28:36 -0400

    add files and react features

---
+++ index.js
@@ -0,0 +1,28 @@
+function App(){
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'learn react',
+      isCompleted: false,
+    },
+    {
+      text: 'meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'build todo app',
+      isCompleted: false,
+    },       
+    {
+      text: 'practice JS functions',
+      isCompleted: false,
+    }      
+  ]);
+
+  return(<>
+     {todos.map((todo, i) => <div key={i}>{todo.text}</div>)}
+    </>);
+}
+
+ReactDOM.render(
+  <App/>, document.getElementById('root')
+);
